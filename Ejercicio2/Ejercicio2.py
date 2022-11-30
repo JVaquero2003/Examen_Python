8b6827b987c8478bbcf58a0de2cfb9ebddd2f53b
JVaquero2003 <95882403+JVaquero2003@users.noreply.github.com>
2022-11-30 10:46:33 +0100

    commit final

--- Ejercicio2/Ejercicio2.py
+++ Ejercicio2/Ejercicio2.py
@@ -38,8 +38,11 @@ def printvocales():
 
 fichero = input("Introduce la ruta de tu fichero: ")
 
-fichero_size = os.path.getsize(fichero)
-print("El fichero", fichero, "tiene un tamaño de", fichero_size , "bytes")
+#fichero_size = os.path.getsize(fichero)
+#print("El fichero", fichero, "tiene un tamaño de", fichero_size , "bytes")
+file_stats = os.stat(fichero)
+print(f'File Size in Bytes is {file_stats.st_size}')
+print(f'File Size in MegaBytes is {file_stats.st_size / (1024 * 1024)}')
 
 
 vocales = 0
